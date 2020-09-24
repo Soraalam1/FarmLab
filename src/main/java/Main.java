39274e6e472f4785a146f17717fb4a4ed003f08a
Dean Alam <s1gamingc@gmail.com>
2020-09-24 15:20:12 -0400

    Set up the main for everybody to work

---
+++ src/main/java/Main.java
@@ -0,0 +1,97 @@
+public class Main {
+    public static void main(String[] args) {
+       //Follow the instructions in the lab. We need a certain number of certain things.
+
+        //Set up People
+        Farmer Froilan = new Farmer("Froilan");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Set up Animals
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Set up Buildings and Fields (Farm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Set up Vehicles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Set up Produce
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
