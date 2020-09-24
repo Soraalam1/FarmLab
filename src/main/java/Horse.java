0175b1852ae9831aa9c0af1770838fde633357e2
yherrera001 <yherrera001@my.wilmu.edu>
2020-09-24 15:29:08 -0400

    I changed the noise that the horse makes

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,9 +1,9 @@
 public class Horse implements Animal, Rideable {
     public void makeNoise(){
-        System.out.println("Horse Noise");
+        System.out.println("YUUUUHHHHEEERRRR");
     }
     public void eat(Edible object){
-        System.out.println(object + "is eaten");
+        System.out.println(object + "chomp chomp chomp");
     }
 
 }
